8da6cd7171b14d11b8a1f63fe7bbb231f4bceb71
Vainqueur <165157345+Vainqueurgab@users.noreply.github.com>
2024-05-02 21:33:01 +0200

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,18 +1,19 @@
 const axios = require('axios');
 
 const Prefixes = [
-  '/ai', 
-  'titus',
-  'nemo',
+  '+ai',
+  'salut',
+  'oliver',
   'ai',
-  'ask',
+  'gabriel',
+  '🍅',
 ];
 
 module.exports = {
   config: {
     name: "ask",
     version: 1.0,
-    author: "OtinXSandip",
+    author: "ZenildeXrene",
     longDescription: "AI",
     category: "ai",
     guide: {
@@ -29,7 +30,7 @@ module.exports = {
       }
       const prompt = event.body.substring(prefix.length).trim();
    if (!prompt) {
-        await message.reply("Bonjour, comment puis-je vous aidez ? 💖");
+        await message.reply("♡ _♫__♡\n║║╔║║╔╗ ♫\n╠╣╠║║║║\n║║╚╚╚╚╝ ♫\𝐧🎯𝐒𝐚𝐥𝐮𝐭 𝐦𝐞𝐬 𝐜𝐡𝐞𝐫𝐬 𝐮𝐭𝐢𝐥𝐢𝐬𝐚𝐭𝐞𝐮𝐫𝐬. 𝐌𝐨𝐧 𝐧𝐨𝐦 𝐞𝐬𝐭 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐮𝐧𝐞 𝐢𝐧𝐭𝐞𝐥𝐥𝐢𝐠𝐞𝐧𝐜𝐞 𝐚𝐫𝐭𝐢𝐟𝐢𝐜𝐢𝐞𝐥𝐥𝐞 𝐜𝐫𝐞́𝐞́𝐞 𝐩𝐚𝐫 ❍❦ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐎𝐥𝐢𝐯𝐞𝐫 ❍❦. 𝐏𝐨𝐬𝐞𝐳 𝐦𝐨𝐢 𝐯𝐨𝐬 𝐪𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐞𝐭 𝐣𝐞 𝐯𝐨𝐮𝐬 𝐨𝐟𝐟𝐫𝐢𝐫𝐚𝐢 𝐝𝐞 𝐦𝐞𝐢𝐥𝐥𝐞𝐮𝐫𝐬 𝐫𝐞́𝐬𝐮𝐥𝐭𝐚𝐭𝐬(✿◠‿◠)\n┈┈┈╲┈┈┈┈╱\n┈┈┈╱▔▔▔▔╲\n┈┈┃┈▇┈┈▇┈┃\n╭╮┣━━━━━━┫╭╮\n┃┃┃┈┈┈┈┈┈┃┃┃\n╰╯┃┈┈┈┈┈┈┃╰╯\n┈┈╰┓┏━━┓┏╯\n┈┈┈╰╯┈┈╰╯\n❦✰𝐆𝐀𝐁𝐑𝐈𝐄𝐋✰❦");
         return;
       }
 
@@ -38,10 +39,16 @@ module.exports = {
       const answer = response.data.answer;
 
  
-    await message.reply(answer);
+    await message.reply({ body: `\n♡ _♫__♡\n║║╔║║╔╗ ♫\n╠╣╠║║║║\n║║╚╚╚╚╝ ♫
+𝐆𝐀𝐁𝐑𝐈𝐄𝐋 🎯
+●══════❍══════●
+${answer} 
+●══════❍══════●
+❦✰𝐆𝐀𝐑𝐑𝐈𝐄𝐋✰❦ `,
+});
 
-    } catch (error) {
+   } catch (error) {
       console.error("Error:", error.message);
     }
   }
-};
+};;
